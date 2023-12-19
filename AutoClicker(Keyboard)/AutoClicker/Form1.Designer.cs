b19d8f918417e5970cc65b072fa426876b61c4dc
Metehan Turgut <metehanturgut794@gmail.com>
2023-12-19 14:21:41 +0300

    .
    .

---
+++ AutoClicker(Keyboard)/AutoClicker/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿using System.Drawing;
+
+namespace AutoClicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.leftMousePanel = new System.Windows.Forms.Panel();
+            this.autoClick = new System.Windows.Forms.CheckBox();
+            this.AutoClickListenerStart = new System.Windows.Forms.Button();
+            this.StartBinding = new System.Windows.Forms.Button();
+            this.CPSLabel = new System.Windows.Forms.Label();
+            this.MouseCPS = new System.Windows.Forms.TrackBar();
+            this.rightClickTimer = new System.Windows.Forms.Timer(this.components);
+            this.leftClickTimer = new System.Windows.Forms.Timer(this.components);
+            this.keyboardWorkerL = new System.ComponentModel.BackgroundWorker();
+            this.keyboardWorkerR = new System.ComponentModel.BackgroundWorker();
+            this.leftClickDetector = new System.ComponentModel.BackgroundWorker();
+            this.rightClickDetector = new System.ComponentModel.BackgroundWorker();
+            this.leftMousePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MouseCPS)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // leftMousePanel
+            // 
+            this.leftMousePanel.Controls.Add(this.autoClick);
+            this.leftMousePanel.Controls.Add(this.AutoClickListenerStart);
+            this.leftMousePanel.Controls.Add(this.StartBinding);
+            this.leftMousePanel.Controls.Add(this.CPSLabel);
+            this.leftMousePanel.Controls.Add(this.MouseCPS);
+            this.leftMousePanel.Location = new System.Drawing.Point(12, 12);
+            this.leftMousePanel.Name = "leftMousePanel";
+            this.leftMousePanel.Size = new System.Drawing.Size(351, 545);
+            this.leftMousePanel.TabIndex = 0;
+            // 
+            // autoClick
+            // 
+            this.autoClick.AutoSize = true;
+            this.autoClick.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.autoClick.ForeColor = System.Drawing.Color.White;
+            this.autoClick.Location = new System.Drawing.Point(273, 46);
+            this.autoClick.Name = "autoClick";
+            this.autoClick.Size = new System.Drawing.Size(75, 29);
+            this.autoClick.TabIndex = 7;
+            this.autoClick.Text = "Auto";
+            this.autoClick.UseVisualStyleBackColor = true;
+            // 
+            // AutoClickListenerStart
+            // 
+            this.AutoClickListenerStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(12)))), ((int)(((byte)(14)))));
+            this.AutoClickListenerStart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.AutoClickListenerStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AutoClickListenerStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.AutoClickListenerStart.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(175)))), ((int)(((byte)(131)))), ((int)(((byte)(53)))));
+            this.AutoClickListenerStart.Location = new System.Drawing.Point(160, 3);
+            this.AutoClickListenerStart.Name = "AutoClickListenerStart";
+            this.AutoClickListenerStart.Size = new System.Drawing.Size(188, 37);
+            this.AutoClickListenerStart.TabIndex = 3;
+            this.AutoClickListenerStart.Text = "Start Pressing";
+            this.AutoClickListenerStart.UseVisualStyleBackColor = false;
+            this.AutoClickListenerStart.Click += new System.EventHandler(this.AutoClickListenerStart_Click);
+            // 
+            // StartBinding
+            // 
+            this.StartBinding.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(12)))), ((int)(((byte)(14)))));
+            this.StartBinding.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.StartBinding.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.StartBinding.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.StartBinding.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(175)))), ((int)(((byte)(131)))), ((int)(((byte)(53)))));
+            this.StartBinding.Location = new System.Drawing.Point(160, 46);
+            this.StartBinding.Name = "StartBinding";
+            this.StartBinding.Size = new System.Drawing.Size(107, 60);
+            this.StartBinding.TabIndex = 2;
+            this.StartBinding.Text = "F";
+            this.StartBinding.UseVisualStyleBackColor = false;
+            this.StartBinding.Click += new System.EventHandler(this.leftMouseBinding_Click);
+            // 
+            // CPSLabel
+            // 
+            this.CPSLabel.AutoSize = true;
+            this.CPSLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(56)))));
+            this.CPSLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.CPSLabel.ForeColor = System.Drawing.Color.White;
+            this.CPSLabel.Location = new System.Drawing.Point(65, 3);
+            this.CPSLabel.Name = "CPSLabel";
+            this.CPSLabel.Size = new System.Drawing.Size(70, 25);
+            this.CPSLabel.TabIndex = 1;
+            this.CPSLabel.Text = "0 CPS";
+            // 
+            // MouseCPS
+            // 
+            this.MouseCPS.Location = new System.Drawing.Point(3, 3);
+            this.MouseCPS.Maximum = 1000;
+            this.MouseCPS.Name = "MouseCPS";
+            this.MouseCPS.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.MouseCPS.Size = new System.Drawing.Size(56, 539);
+            this.MouseCPS.TabIndex = 0;
+            this.MouseCPS.Scroll += new System.EventHandler(this.CPS_Scroll);
+            // 
+            // leftClickTimer
+            // 
+            this.leftClickTimer.Interval = 1000;
+            this.leftClickTimer.Tick += new System.EventHandler(this.ClickTimer_Tick);
+            // 
+            // keyboardWorkerL
+            // 
+            this.keyboardWorkerL.WorkerSupportsCancellation = true;
+            this.keyboardWorkerL.DoWork += new System.ComponentModel.DoWorkEventHandler(this.keyboardWorkerL_DoWork);
+            // 
+            // leftClickDetector
+            // 
+            this.leftClickDetector.WorkerSupportsCancellation = true;
+            this.leftClickDetector.DoWork += new System.ComponentModel.DoWorkEventHandler(this.leftClickDetector_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(56)))));
+            this.ClientSize = new System.Drawing.Size(381, 569);
+            this.Controls.Add(this.leftMousePanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Clicker";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.leftMousePanel.ResumeLayout(false);
+            this.leftMousePanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MouseCPS)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel leftMousePanel;
+        private System.Windows.Forms.Label CPSLabel;
+        private System.Windows.Forms.Button StartBinding;
+        private System.Windows.Forms.Button AutoClickListenerStart;
+        private System.Windows.Forms.Timer rightClickTimer;
+        private System.Windows.Forms.Timer leftClickTimer;
+        private System.ComponentModel.BackgroundWorker keyboardWorkerL;
+        private System.ComponentModel.BackgroundWorker keyboardWorkerR;
+        private System.ComponentModel.BackgroundWorker leftClickDetector;
+        private System.ComponentModel.BackgroundWorker rightClickDetector;
+        private System.Windows.Forms.CheckBox autoClick;
+        private System.Windows.Forms.TrackBar MouseCPS;
+    }
+}
+
---
+++ AutoClicker(Keyboard)/AutoClicker/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Windows.Input;
+using Keyboard = System.Windows.Input.Keyboard;
+using System.Runtime.CompilerServices;
+using System.Drawing;
+using System.Diagnostics.Eventing.Reader;
+
+namespace AutoClicker
+{
+    public partial class Form1 : Form
+    {
+        public string StartStopKey = "F";
+        public bool Start = false, keyPress = false, auto = false;
+        Thread th;
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+            keyboardWorkerL.RunWorkerAsync();
+            keyboardWorkerR.RunWorkerAsync();
+            leftClickDetector.RunWorkerAsync();
+            rightClickDetector.RunWorkerAsync();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StartBinding.Text = StartStopKey;
+        }
+
+        [DllImport("User32.dll")]
+        public static extern short GetAsyncKeyState(Keys ArrowKeys);
+
+
+
+        bool isRunningL = false;
+        private void AutoClickListenerStart_Click(object sender, EventArgs e)
+        {
+            if (AutoClickListenerStart.Text.Contains("Start"))
+            {
+                if (MouseCPS.Value != 0)
+                {
+                    AutoClickListenerStart.Text = "Stop Pressing";
+
+                    leftClickTimer.Interval = 100000 / (MouseCPS.Value);
+                    leftClickTimer.Start();
+                    isRunningL = true;
+                }
+            }
+            else
+            {
+                AutoClickListenerStart.Text = "Start Pressing";
+                AutoClickListenerStart.BackColor = Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(12)))), ((int)(((byte)(14)))));
+                AutoClickListenerStart.ForeColor = Color.FromArgb(((int)(((byte)(175)))), ((int)(((byte)(131)))), ((int)(((byte)(53)))));
+                Start = false;
+                isRunningL = false;
+            }
+        }
+
+        [DllImport("user32.dll")]
+        static extern void mouse_event(uint dwFlags, int dx, int dy, uint dwData, UIntPtr dwExtraInfo);
+
+
+        [DllImport("user32.dll")] 
+        static extern uint keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+
+        public static void KeyDown(Keys key)
+        {
+            keybd_event((byte)key, 0, 0, 0);
+        }
+
+        public static void KeyUp(Keys key)
+        {
+            keybd_event((byte)key, 0, 0x0002, 0);
+        }
+
+        const uint LeftD = 0x0002;
+        const uint LeftU = 0x0004;
+
+
+
+        [DllImport("user32.dll")]
+        public static extern bool GetAsyncKeyState(int button);
+
+
+        public static bool IsMouseButtonPressed(MouseButton button)
+        {
+            return GetAsyncKeyState((int)button);
+        }
+
+        public enum MouseButton
+        {
+            LeftMouseButton = 0x01,
+            RightMouseButton = 0x02,
+            MiddleMouseButton = 0x04,
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+            Application.Exit();
+        }
+
+        private void CPS_Scroll(object sender, EventArgs e)
+        {
+            CPSLabel.Text = (float)(MouseCPS.Value)/100 + " CPS";
+        }
+        private void ClickTimer_Tick(object sender, EventArgs e)
+        {
+            if (!autoClick.Checked && keyPress && Start)
+            {
+                KeyDown(Keys.D7);
+                Thread.Sleep(1);
+                KeyUp(Keys.D7);
+                Thread.Sleep(5);
+                KeyDown(Keys.D8);
+                Thread.Sleep(1);
+                KeyUp(Keys.D8);
+            }
+            else if (autoClick.Checked && Start)
+            {
+                KeyDown(Keys.D7);
+                Thread.Sleep(1);
+                KeyUp(Keys.D7);
+                Thread.Sleep(5);
+                KeyDown(Keys.D8);
+                Thread.Sleep(1);
+                KeyUp(Keys.D8);
+            }
+            else { return; }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(0);
+            Application.Exit();
+        }
+
+        private void leftClickDetector_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                if (IsMouseButtonPressed(MouseButton.LeftMouseButton))
+                {
+                    keyPress = true;
+                }
+                else
+                {
+                    keyPress = false;
+                }
+            }
+        }
+
+        private void keyboardWorkerL_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                if (isRunningL)
+                {
+                    if (GetAsyncKeyState((Keys)Char.Parse(StartStopKey)) < 0)
+                    {
+                        if (Start)
+                        {
+                            AutoClickListenerStart.BackColor = Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(12)))), ((int)(((byte)(14)))));
+                            AutoClickListenerStart.ForeColor = Color.FromArgb(((int)(((byte)(175)))), ((int)(((byte)(131)))), ((int)(((byte)(53)))));
+                            Start = false;
+                            Thread.Sleep(300);
+                        }
+                        else
+                        {
+                            AutoClickListenerStart.BackColor = System.Drawing.Color.Green;
+                            AutoClickListenerStart.ForeColor = System.Drawing.Color.White;
+                            Start = true;
+                            Thread.Sleep(300);
+                        }
+                    }
+                }
+            }
+        }
+        private void leftMouseBinding_Click(object sender, EventArgs e)
+        {
+            StartStopKey = Interaction.InputBox("Start/Stop Key", "Give Your Start Stop Key", "F", 0, 0);
+            StartStopKey = StartStopKey.ToUpper();
+            StartBinding.Text = StartStopKey;
+        }
+    }
+}
